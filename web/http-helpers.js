988326128036373c1ed67b332841395bd41ad303
David Churchill <dchurchill92@gmail.com>
2015-09-10 14:20:55 -0500

    implement sendResponse helper

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -2,7 +2,7 @@ var path = require('path');
 var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
 
-exports.headers = headers = {
+defaultHeaders = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
   "access-control-allow-headers": "content-type, accept",
@@ -14,6 +14,15 @@ exports.serveAssets = function(res, asset, callback) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...),
   // css, or anything that doesn't change often.)
+};
+
+
+exports.sendResponse = function(res, data, statusCode, headers) {
+  statusCode = statusCode || 200;
+  headers = headers || defaultHeaders;
+  res.writeHead(statusCode, headers);
+  res.write(data);
+  res.end(archive.paths.list);
 };
 
 
